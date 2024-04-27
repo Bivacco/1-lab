1614be2119678e1870d0f9bf5e0b8eb52542ae85
Bivacco <166908313+Bivacco@users.noreply.github.com>
2024-04-27 13:17:28 +0400

    1lab.py

--- 1.py
+++ 1.py
@@ -1,28 +1,47 @@
-def remove_odd_digits(num):
-    even_nums = [int(x) for x in str(num) if int(x) % 2 == 0]
-    removed_nums = [int(x) for x in str(num) if int(x) % 2 != 0]
-    print(f"Из числа {num} удалены нечетные цифры: {' '.join(str(x) for x in removed_nums)}")
-    return int(''.join(str(x) for x in even_nums))
+'''
+Натуральные числа.
+Выводит на экран числа, убирая нечетные цифры в каждом
+четном по порядку числе. Убранные цифры печать отдельно прописью.
+'''
+#Словарь с цифрами
+dc_cifr = {'0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре', '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять'}
+#Баффер
+gived_num = '1'
+#Выбор файла
+file_name = "text.txt"
+#Номер числа по порядку
+i = 0
 
-def process_sequence(sequence):
-    current_num = ""
-    found_number = False
-    for char in sequence:
-        if char.isdigit():
-            current_num += char
-        else:
-            if current_num:
-                num = int(current_num)
-                if num % 2 == 0:
-                    found_number = True
-                    result = remove_odd_digits(num)
-                    print(f"Число после удаления нечетных цифр: {result}\n")
-                current_num = ""
-    if not found_number:
-        print("Файл пустой или не содержит натуральных чисел.")
+try:
+    open(file_name, 'r')
+except:
+    print('Файл отсутствует в директории проекта')
+    exit()
 
-# Чтение последовательности из файла
-with open('sequence.txt', 'r') as file:
-    sequence = file.read()
+with open(file_name, 'r') as file:
+    while 1:
+        i+=1
+        gived_num = file.readline().replace('\n', '')
+        if not gived_num:
+            #print('Файл закончился')
+            break
+        if not gived_num.isdigit():
+            continue
 
-process_sequence(sequence)
+        print()
+        print(gived_num)
+        cutted =  ''
+
+        if i % 2 == 0:
+            for j in gived_num:
+                if int(j) % 2 == 0:
+                    cutted += j
+            if cutted == gived_num:
+                continue
+
+            print(cutted)
+
+            for j in gived_num:
+                if int(j) % 2 != 0:
+                    print(dc_cifr[j], end='  ')
+            print()
